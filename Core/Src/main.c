d3e6a73162a1be67e38a1063177d7bdf96928566
zhixiang <zhou.zhixiang@outlook.com>
2025-12-21 07:18:21 +0800

    feat: fmc cubemx 配置

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -27,6 +27,7 @@
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
+#include "fsmc.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -113,6 +114,7 @@ int main(void)
   MX_ADC1_Init();
   MX_DAC_Init();
   MX_TIM4_Init();
+  MX_FSMC_Init();
   /* USER CODE BEGIN 2 */
   dac_test();
   /* USER CODE END 2 */
